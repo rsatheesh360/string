f73c2036d015d027ebd93a8eb9a33eb4901be123
R.Satheesh <36471895+rsatheesh360@users.noreply.github.com>
2018-03-22 13:04:11 +0530

    strcmp

---
+++ .gitignore/string.py
@@ -0,0 +1,16 @@
+def compare(str1,str2):
+	if str1==str2:
+		print(str2)
+	elif str1>str2:
+		print(str1)
+	else :
+		print(str2)
+def main():
+	try:
+		s1=input()
+		s2=input()
+		compare(s1,s2)
+	except:
+		print('invalid')
+main()
+   print(n)
